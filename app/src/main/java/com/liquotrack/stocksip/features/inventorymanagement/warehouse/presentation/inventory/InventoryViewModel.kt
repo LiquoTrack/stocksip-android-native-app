ce44fe813c616154bda63beca5660aeaef70bbdc
Nicolas Juarez <juarezleonn2000@gmail.com>
2025-09-29 20:30:18 -0500

    chore: add base entities

---
+++ app/src/main/java/com/liquotrack/stocksip/features/inventorymanagement/warehouse/presentation/inventory/InventoryViewModel.kt
@@ -0,0 +1,7 @@
+package com.liquotrack.stocksip.features.inventorymanagement.warehouse.presentation.inventory
+
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+@HiltViewModel
+class InventoryViewModel {
+}
