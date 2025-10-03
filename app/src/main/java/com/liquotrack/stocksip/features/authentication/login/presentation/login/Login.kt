d2ad96556555a315a1d4ef3942a48ff62895ae29
Nicolas Juarez <juarezleonn2000@gmail.com>
2025-10-03 17:26:17 -0500

    chore: add login section base classes

---
+++ app/src/main/java/com/liquotrack/stocksip/features/authentication/login/presentation/login/Login.kt
@@ -0,0 +1,8 @@
+package com.liquotrack.stocksip.features.authentication.login.presentation.login
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun Login() {
+
+}
---
+++ app/src/main/java/com/liquotrack/stocksip/features/authentication/login/presentation/login/LoginViewModel.kt
@@ -0,0 +1,7 @@
+package com.liquotrack.stocksip.features.authentication.login.presentation.login
+
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+@HiltViewModel
+class LoginViewModel {
+}
