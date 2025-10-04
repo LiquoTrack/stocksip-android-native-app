0438698eb7d20fc95eb960b7db48ffceab1e9d10
Camila Huamani <jyb990106@gmail.com>
2025-10-03 19:53:45 -0500

    feat(authentication): add login and register presentation screens.

--- app/src/main/java/com/liquotrack/stocksip/features/authentication/login/presentation/login/LoginViewModel.kt
+++
@@ -1,7 +0,0 @@
-package com.liquotrack.stocksip.features.authentication.login.presentation.login
-
-import dagger.hilt.android.lifecycle.HiltViewModel
-
-@HiltViewModel
-class LoginViewModel {
-}
--- app/src/main/java/com/liquotrack/stocksip/features/inventorymanagement/warehouse/presentation/warehouse/WarehouseViewModel.kt
+++
@@ -1,7 +0,0 @@
-package com.liquotrack.stocksip.features.inventorymanagement.warehouse.presentation.warehouse
-
-import dagger.hilt.android.lifecycle.HiltViewModel
-
-@HiltViewModel
-class WarehouseViewModel {
-}
