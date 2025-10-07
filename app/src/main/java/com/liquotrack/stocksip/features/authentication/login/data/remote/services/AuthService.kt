2c22bc38fd5213d5bc274034fbfe56dc709ce451
Camila Huamani <jyb990106@gmail.com>
2025-10-07 16:55:33 -0500

    feat(authentication): add authentication service.

---
+++ app/src/main/java/com/liquotrack/stocksip/features/authentication/login/data/remote/services/AuthService.kt
@@ -0,0 +1,18 @@
+package com.liquotrack.stocksip.features.authentication.login.data.remote.services
+
+import com.liquotrack.stocksip.features.authentication.login.data.remote.model.SignInRequestDto
+import com.liquotrack.stocksip.features.authentication.login.data.remote.model.SignInResponseDto
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.POST
+
+interface AuthService {
+    @POST("api/v1/authentication/sign-in")
+    suspend fun login(@Body request: SignInRequestDto): Response<SignInResponseDto>
+
+    @POST("api/v1/authentication/sign-out")
+    suspend fun logout(): Response<Unit>
+
+    @POST("api/v1/authentication/forgot-password")
+    suspend fun forgotPassword(@Body email: String): Response<Unit>
+}
---
+++ app/src/main/java/com/liquotrack/stocksip/features/authentication/login/data/repositories/AuthRepositoryImpl.kt
@@ -0,0 +1,64 @@
+package com.liquotrack.stocksip.features.authentication.login.data.repositories
+
+import com.liquotrack.stocksip.common.utils.Resource
+import com.liquotrack.stocksip.features.authentication.login.data.remote.model.SignInRequestDto
+import com.liquotrack.stocksip.features.authentication.login.data.remote.services.AuthService
+import com.liquotrack.stocksip.features.authentication.login.domain.model.User
+import com.liquotrack.stocksip.features.authentication.login.domain.repositories.AuthRepository
+import com.liquotrack.stocksip.shared.data.local.TokenManager
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
+
+class AuthRepositoryImpl @Inject constructor(
+    private val service: AuthService,
+    private val tokenManager: TokenManager
+) : AuthRepository {
+
+    override suspend fun login(email: String, password: String): Resource<User> =
+        withContext(Dispatchers.IO) {
+            try {
+                val response = service.login(SignInRequestDto(email = email, password = password))
+
+                if (response.isSuccessful) {
+                    response.body()?.let { loginResponse ->
+                        val user = User(
+                            email = loginResponse.user.email,
+                            username = loginResponse.user.username,
+                            userRole = loginResponse.user.userRole,
+                            accountId = loginResponse.user.accountId
+                        )
+                        tokenManager.saveToken(loginResponse.token)
+                        return@withContext Resource.Success(data = user)
+                    }
+                }
+                return@withContext Resource.Error("Unknown error")
+            } catch (e: Exception) {
+                return@withContext Resource.Error(e.localizedMessage ?: "Connection error")
+            }
+        }
+
+    override suspend fun logout(): Resource<Unit> =
+        withContext(Dispatchers.IO) {
+            try {
+                tokenManager.clearTokens()
+                return@withContext Resource.Success(Unit)
+            } catch (e: Exception) {
+                return@withContext Resource.Error(e.localizedMessage ?: "Error logging out")
+            }
+        }
+
+    override suspend fun forgotPassword(email: String): Resource<Unit> =
+        withContext(Dispatchers.IO) {
+            try {
+                val response = service.forgotPassword(email)
+
+                if (response.isSuccessful) {
+                    return@withContext Resource.Success(Unit)
+                }
+                return@withContext Resource.Error("Failed to send recovery email")
+            } catch (e: Exception) {
+                return@withContext Resource.Error(e.localizedMessage ?: "Connection error")
+            }
+        }
+}
