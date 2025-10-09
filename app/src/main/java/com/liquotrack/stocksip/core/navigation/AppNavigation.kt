f74c266ba90603ad446a7ba277940aff531c553e
Camila Huamani <jyb990106@gmail.com>
2025-10-09 00:49:31 -0500

    fix: update navigation.

--- app/src/main/java/com/liquotrack/stocksip/core/navigation/AppNavigation.kt
+++ app/src/main/java/com/liquotrack/stocksip/core/navigation/AppNavigation.kt
@@ -8,21 +8,24 @@ import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
 import com.liquotrack.stocksip.features.adminpanel.presentation.AdminPanel
 import com.liquotrack.stocksip.features.authentication.login.presentation.login.Login
+import com.liquotrack.stocksip.features.authentication.passwordrecover.presentation.ConfirmationCode
+import com.liquotrack.stocksip.features.authentication.passwordrecover.presentation.RecoverPassword
 import com.liquotrack.stocksip.features.authentication.register.presentation.register.RegisterAccount
 import com.liquotrack.stocksip.features.authentication.register.presentation.register.RegisterUser
+import com.liquotrack.stocksip.features.inventorymanagement.warehouse.presentation.warehouse.WarehouseView
 import com.liquotrack.stocksip.features.profilemanagement.profile.presentation.Profile
 
 /**
- * Composable function that sets up the navigation for the application.
- * Simplified version for testing without authentication requirements.
+ * Composable function that sets up the navigation graph for the app.
+ * This version includes authentication, profile, warehouse, and admin panel routes.
  */
 @Composable
 fun AppNavigation() {
 
-    // Create a NavController instance to handle navigation
+    // NavController instance to handle navigation actions
     val navController = rememberNavController()
 
-    // Define the navigation graph using NavHost
+    // Define all navigation destinations using NavHost
     NavHost(navController, startDestination = Route.Profile.route) {
 
         // Login screen
@@ -30,6 +33,9 @@ fun AppNavigation() {
             Login(
                 onNavigateToRegister = {
                     navController.navigate(Route.Register.route)
+                },
+                onNavigateToRecovery = {
+                    navController.navigate(Route.PasswordRecovery.route)
                 },
                 onLoginSuccess = {
                     navController.navigate(Route.Main.route) {
@@ -74,42 +80,35 @@ fun AppNavigation() {
             )
         }
 
-        // Main screen (Home/Dashboard)
-        composable(route = Route.Main.route) {
-            // TODO: Implement HomeScreen
+        // Password Recovery screen
+        composable(route = Route.PasswordRecovery.route) {
+            RecoverPassword(
+                onNavigateToConfirmation = { email ->
+                    val route = "confirmation_code/$email"
+                    navController.navigate(route)
+                },
+                onNavigateBack = {
+                    navController.popBackStack()
+                }
+            )
         }
 
-        // Warehouses screen
-        composable(route = Route.Warehouses.route) {
-            // TODO: Implement WarehousesScreen
-        }
+        // Confirmation Code screen
+        composable(
+            route = Route.ConfirmationCode.routeWithArguments,
+            arguments = listOf(
+                navArgument(Route.ConfirmationCode.emailArg) { type = NavType.StringType }
+            )
+        ) { backStackEntry ->
+            val email = backStackEntry.arguments?.getString(Route.ConfirmationCode.emailArg) ?: ""
 
-        // Products/Storage screen
-        composable(route = Route.Products.route) {
-            // TODO: Implement ProductsScreen
-        }
-
-        // Alerts screen
-        composable(route = Route.Alerts.route) {
-            // TODO: Implement AlertsScreen
-        }
-
-        // Catalog screen
-        composable(route = Route.Catalogs.route) {
-            // TODO: Implement CatalogsScreen
-        }
-
-        // Care Guides screen
-        composable(route = Route.CareGuides.route) {
-            // TODO: Implement CareGuidesScreen
-        }
-
-        // User Management screen (Admin Panel)
-        composable(route = Route.UserManagement.route) {
-            AdminPanel(
-                onNavigate = { route ->
-                    navController.navigate(route) {
-                        launchSingleTop = true
+            ConfirmationCode(
+                onNavigateBack = {
+                    navController.popBackStack()
+                },
+                onNavigateToLogin = {
+                    navController.navigate(Route.Login.route) {
+                        popUpTo(0) { inclusive = true }
                     }
                 }
             )
@@ -126,29 +125,38 @@ fun AppNavigation() {
             )
         }
 
-        // Plans screen
-        composable(route = Route.Plans.route) {
-            // TODO: Implement PlansScreen
+        // Warehouse screen
+        composable(route = Route.Warehouses.route) {
+            WarehouseView(
+                onNavigate = { route ->
+                    navController.navigate(route) {
+                        launchSingleTop = true
+                    }
+                }
+            )
         }
 
-        // Making Orders screen
-        composable(route = Route.MakingOrders.route) {
-            // TODO: Implement MakingOrdersScreen
+        // Admin Panel screen
+        composable(route = Route.UserManagement.route) {
+            AdminPanel(
+                onNavigate = { route ->
+                    navController.navigate(route) {
+                        launchSingleTop = true
+                    }
+                }
+            )
         }
 
-        // Order History screen
-        composable(route = Route.OrderHistory.route) {
-            // TODO: Implement OrderHistoryScreen
-        }
-
-        // Product Transfer History screen
-        composable(route = Route.ProductTransferHistory.route) {
-            // TODO: Implement ProductTransferHistoryScreen
-        }
-
-        // Product Exit History screen
-        composable(route = Route.ProductExitHistory.route) {
-            // TODO: Implement ProductExitHistoryScreen
-        }
+        // Main and placeholder routes (to be implemented)
+        composable(route = Route.Main.route) { /* TODO: Implement MainScreen */ }
+        composable(route = Route.Products.route) { /* TODO: Implement ProductsScreen */ }
+        composable(route = Route.Alerts.route) { /* TODO: Implement AlertsScreen */ }
+        composable(route = Route.Catalogs.route) { /* TODO: Implement CatalogsScreen */ }
+        composable(route = Route.CareGuides.route) { /* TODO: Implement CareGuidesScreen */ }
+        composable(route = Route.Plans.route) { /* TODO: Implement PlansScreen */ }
+        composable(route = Route.MakingOrders.route) { /* TODO: Implement MakingOrdersScreen */ }
+        composable(route = Route.OrderHistory.route) { /* TODO: Implement OrderHistoryScreen */ }
+        composable(route = Route.ProductTransferHistory.route) { /* TODO: Implement ProductTransferHistoryScreen */ }
+        composable(route = Route.ProductExitHistory.route) { /* TODO: Implement ProductExitHistoryScreen */ }
     }
 }
