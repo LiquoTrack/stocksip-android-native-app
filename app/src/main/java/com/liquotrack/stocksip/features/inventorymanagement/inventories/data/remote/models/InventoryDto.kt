929a439cc9a200a097b724dedd6bc31e6de3333e
Nicolas Juarez <juarezleonn2000@gmail.com>
2025-11-12 12:16:14 -0500

    feat(inventories): add inventory dto objects

---
+++ app/src/main/java/com/liquotrack/stocksip/features/inventorymanagement/inventories/data/remote/models/InventoryDto.kt
@@ -0,0 +1,6 @@
+package com.liquotrack.stocksip.features.inventorymanagement.inventories.data.remote.models
+
+/**
+ * DTO representing a list of inventory items.
+ */
+class InventoryDto : ArrayList<InventoryDtoItem>()
