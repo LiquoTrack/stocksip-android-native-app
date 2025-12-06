40e15f568aee76b520e18a32d7a2d98ac5b4649d
Camila Huamani <jyb990106@gmail.com>
2025-12-05 22:32:13 -0500

    feat(sales-orders): enhance sales order creation by including catalog ID from purchase order

--- app/src/main/java/com/liquotrack/stocksip/features/ordermanagement/data/remote/services/SalesOrderService.kt
+++ app/src/main/java/com/liquotrack/stocksip/features/ordermanagement/data/remote/services/SalesOrderService.kt
@@ -1,25 +1,19 @@
 package com.liquotrack.stocksip.features.ordermanagement.data.remote.services
 
-import com.liquotrack.stocksip.features.ordermanagement.data.remote.models.CreateSalesOrderRequestDto
-import com.liquotrack.stocksip.features.ordermanagement.data.remote.models.OrderStatusResponseDto
-import com.liquotrack.stocksip.features.ordermanagement.data.remote.models.ProposeDeliveryScheduleRequestDto
-import com.liquotrack.stocksip.features.ordermanagement.data.remote.models.RespondDeliveryProposalRequestDto
 import com.liquotrack.stocksip.features.ordermanagement.data.remote.models.SalesOrderDto
-import com.liquotrack.stocksip.features.ordermanagement.data.remote.models.SupplierOrdersResponseDto
-import com.liquotrack.stocksip.features.ordermanagement.data.remote.models.UpdateOrderStatusRequestDto
-import com.liquotrack.stocksip.features.ordermanagement.data.remote.models.LiquorStoreOwnerOrdersResponseDto
 import retrofit2.Response
-import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.POST
-import retrofit2.http.PUT
 import retrofit2.http.Path
+
+import retrofit2.http.Query
 
 interface SalesOrderService {
 
     @POST("orders/from-procurement/{purchaseOrderId}")
     suspend fun createSalesOrderFromPurchaseOrder(
-        @Path("purchaseOrderId") purchaseOrderId: String
+        @Path("purchaseOrderId") purchaseOrderId: String,
+        @Query("catalogId") catalogIdBuyFrom: String?
     ): Response<SalesOrderDto>
 
     @GET("orders")
